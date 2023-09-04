3b228ecfd9fa668ddb81bae8925b5add691dfeea
Jordy de Jonghe <jordy.dejonghe@icapps.com>
2023-09-04 17:15:59 +0200

    #130: added enum model tests

--- test/writer/enum_model_writer_test.dart
+++ test/writer/enum_model_writer_test.dart
@@ -1,5 +1,44 @@
+import 'dart:io';
+
+import 'package:model_generator/config/pubspec_config.dart';
+import 'package:model_generator/config/yml_generator_config.dart';
+import 'package:model_generator/model/model/enum_model.dart';
+import 'package:model_generator/writer/enum_model_writer.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('EnumModel', () {});
+  void testEnumModelWriter(String path) {
+    final file = File('$path/output.txt');
+    final pubspecFile = File('test/writer/enum_model_writer/pubspec.txt');
+    final configFile = File('$path/config.txt');
+    final expected = file.readAsStringSync();
+    final pubspecContent = pubspecFile.readAsStringSync();
+    final configContent = configFile.readAsStringSync();
+    final pubspecConfig = PubspecConfig(pubspecContent);
+    final ymlConfig = YmlGeneratorConfig(pubspecConfig, configContent, '');
+    final jsonModel = ymlConfig.models.first;
+    if (jsonModel is! EnumModel) {
+      throw Exception('The first model in the config file must be an object model and will be validated. The model is ${ymlConfig.models.first.runtimeType}');
+    }
+
+    final generateActual = EnumModelWriter(jsonModel).write;
+    if (expected.startsWith('Exception')) {
+      expect(generateActual, throwsA(isA<Exception>()));
+    } else {
+      expect(generateActual(), expected);
+    }
+  }
+
+  group('EnumModelWriter test', () {
+    final directory = Directory('test/writer/enum_model_writer');
+    final folders = directory.listSync();
+    for (final folder in folders) {
+      if (folder is Directory) {
+        test('Folder ${folder.path}', () {
+          print('Testing folder ${folder.path}');
+          testEnumModelWriter(folder.path);
+        });
+      }
+    }
+  });
 }
